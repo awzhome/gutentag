82a0d471d23543216e55bd64807fce4666cc3029
Andreas Weizel <rpinski@awzhome.de>
2021-06-17 23:42:02 +0200

    Introduce unit tests for Versioning class

---
+++ AWZhome.GutenTag.Tests/MockedGitExecutor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AWZhome.GutenTag.Tests
+{
+    public class MockedGitExecutor
+    {
+        public const string HasCleanWorkingCopyCmdLine = "status --short";
+        public const string CurrentBranchCmdLine = "rev-parse --abbrev-ref HEAD";
+
+        public string SimpleDescribeCmdLine => $"describe --first-parent --match \"{devTagPrefix}*\" --match \"{releaseTagPrefix}*\"";
+
+        public string DescribeOnlyPatchMatchesCmdLine =>
+           "describe --first-parent " + string.Join(' ', new[] {
+               $"{devTagPrefix}[0-9999]", $"{releaseTagPrefix}[0-9999]", $"{devTagPrefix}[0-9999].[0-9999]", $"{releaseTagPrefix}[0-9999].[0-9999]", $"{devTagPrefix}[0-9999].[0-9999].[0-9999]", $"{releaseTagPrefix}[0-9999].[0-9999].[0-9999]"
+           }.Select(m => $"--match \"{m}\""));
+
+        public string DescribeOnlyMinorMatchesCmdLine =>
+           "describe --first-parent " + string.Join(' ', new[] {
+               $"{devTagPrefix}[0-9999]", $"{releaseTagPrefix}[0-9999]", $"{devTagPrefix}[0-9999].[0-9999]", $"{releaseTagPrefix}[0-9999].[0-9999]", $"{devTagPrefix}[0-9999].[0-9999].0", $"{releaseTagPrefix}[0-9999].[0-9999].0"
+           }.Select(m => $"--match \"{m}\""));
+
+        public Dictionary<string, IEnumerable<string>> MockedOutput { get; } = new();
+
+        private readonly string releaseTagPrefix = "v";
+        private readonly string devTagPrefix = "dev-";
+
+        public MockedGitExecutor(VersioningConfig versioningConfig)
+        {
+            releaseTagPrefix = versioningConfig.ReleaseTagPrefix;
+            devTagPrefix = versioningConfig.DevTagPrefix;
+        }
+
+        public bool HasCleanWorkingCopy
+        {
+            set => MockedOutput[HasCleanWorkingCopyCmdLine] = value ? Enumerable.Empty<string>() : new[] { "file", "anotherfile" };
+        }
+
+        public string CurrentBranch
+        {
+            set => MockedOutput[CurrentBranchCmdLine] = new[] { value };
+        }
+
+        public string ResultOnSimpleDescribe
+        {
+            set => MockedOutput[SimpleDescribeCmdLine] = new[] { value };
+        }
+
+        public string ResultOnDescribeOnlyPatchMatches
+        {
+            set => MockedOutput[DescribeOnlyPatchMatchesCmdLine] = new[] { value };
+        }
+
+        public (string Excluding, string Result) ResultOnDescribeOnlyPatchMatchesWithExclude
+        {
+            set => MockedOutput[DescribeOnlyPatchMatchesCmdLine + $" --exclude \"{value.Excluding}\""] = new[] { value.Result };
+        }
+
+        public string ResultOnDescribeOnlyMinorMatches
+        {
+            set => MockedOutput[DescribeOnlyMinorMatchesCmdLine] = new[] { value };
+        }
+
+        public (string Excluding, string Result) ResultOnDescribeOnlyMinorMatchesWithExclude
+        {
+            set => MockedOutput[DescribeOnlyMinorMatchesCmdLine + $" --exclude \"{value.Excluding}\""] = new[] { value.Result };
+        }
+
+        public IEnumerable<string> Execute(string commandLine) =>
+            MockedOutput.GetValueOrDefault(commandLine.Trim());
+
+        public static implicit operator GitExecutor(MockedGitExecutor mockedGitExcecutor)
+        {
+            return mockedGitExcecutor.Execute;
+        }
+    }
+}
---
+++ AWZhome.GutenTag.Tests/VersioningTests.cs
@@ -0,0 +1,384 @@
+﻿using Xunit;
+
+namespace AWZhome.GutenTag.Tests
+{
+    public class VersioningTests
+    {
+        [Fact]
+        public void MajorRelease()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0",
+                ResultOnDescribeOnlyPatchMatches = "v2.0"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(0, version.PreReleaseNumber);
+            Assert.Equal(0, version.Revision);
+            Assert.Null(version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void MajorRelease_DifferentPrefix()
+        {
+            VersioningConfig versioningConfig = new() { ReleaseTagPrefix = "ver-" };
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "ver-2.0",
+                ResultOnDescribeOnlyPatchMatches = "ver-2.0"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("ver-2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(0, version.PreReleaseNumber);
+            Assert.Equal(0, version.Revision);
+            Assert.Null(version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void PatchRelease()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0.1",
+                ResultOnDescribeOnlyPatchMatches = "v2.0.1"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0.1", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(1, version.Patch);
+            Assert.Equal(0, version.PreReleaseNumber);
+            Assert.Equal(0, version.Revision);
+            Assert.Null(version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void MajorRelease_MinorIncrement_DirtyWorkingCopy()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = false };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = false,
+                ResultOnSimpleDescribe = "v2.0",
+                ResultOnDescribeOnlyMinorMatches = "v2.0"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(1, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(1, version.PreReleaseNumber);
+            Assert.Equal(1, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void MajorRelease_PatchIncrement_DirtyWorkingCopy()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = false,
+                ResultOnSimpleDescribe = "v2.0",
+                ResultOnDescribeOnlyPatchMatches = "v2.0"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(1, version.Patch);
+            Assert.Equal(1, version.PreReleaseNumber);
+            Assert.Equal(1, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void CommitAfterMajorRelease_MinorIncrement_CleanWorkingCopy()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = false };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0-1-abcdef",
+                ResultOnDescribeOnlyMinorMatches = "v2.0-1-abcdef"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(1, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(1, version.PreReleaseNumber);
+            Assert.Equal(1, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void CommitAfterMajorRelease_MinorIncrement_DirtyWorkingCopy()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = false };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = false,
+                ResultOnSimpleDescribe = "v2.0-1-abcdef",
+                ResultOnDescribeOnlyMinorMatches = "v2.0-1-abcdef"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(1, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(2, version.PreReleaseNumber);
+            Assert.Equal(2, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void CommitAfterMajorRelease_PatchIncrement_CleanWorkingCopy()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0-1-abcdef",
+                ResultOnDescribeOnlyPatchMatches = "v2.0-1-abcdef"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(1, version.Patch);
+            Assert.Equal(1, version.PreReleaseNumber);
+            Assert.Equal(1, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void CommitOnDevMark()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "dev-2.0",
+                ResultOnDescribeOnlyPatchMatches = "dev-2.0"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("dev-2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(1, version.PreReleaseNumber);
+            Assert.Equal(1, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.True(version.IsDevMark);
+        }
+
+        [Fact]
+        public void CommitAfterDevMark()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "dev-2.0-1-abcdef",
+                ResultOnDescribeOnlyPatchMatches = "dev-2.0-1-abcdef"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("dev-2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(2, version.PreReleaseNumber);
+            Assert.Equal(2, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.True(version.IsDevMark);
+        }
+
+        [Fact]
+        public void MajorReleaseAfterDevMarkForSameVersion()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0",
+                ResultOnDescribeOnlyPatchMatches = "v2.0",
+                ResultOnDescribeOnlyPatchMatchesWithExclude = (Excluding: "v2.0", Result: "dev-2.0-5-abcdef")
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(0, version.PreReleaseNumber);
+            Assert.Equal(6, version.Revision);
+            Assert.Null(version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void MajorReleaseAfterDevMarkForSameVersion_DifferentPrefixes()
+        {
+            VersioningConfig versioningConfig = new() { ReleaseTagPrefix = "ver-", DevTagPrefix = "start-" };
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "ver-2.0",
+                ResultOnDescribeOnlyPatchMatches = "ver-2.0",
+                ResultOnDescribeOnlyPatchMatchesWithExclude = (Excluding: "ver-2.0", Result: "start-2.0-5-abcdef")
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("ver-2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(0, version.PreReleaseNumber);
+            Assert.Equal(6, version.Revision);
+            Assert.Null(version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void PatchReleaseAfterMajorRelease_MinorIncrement()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = false };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0.1",
+                ResultOnDescribeOnlyMinorMatches = "v2.0-5-abcdef"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0.1", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(1, version.Patch);
+            Assert.Equal(0, version.PreReleaseNumber);
+            Assert.Equal(0, version.Revision);
+            Assert.Null(version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void AfterPatchReleaseAfterMajorRelease_PatchIncrement()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = true };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0.1-2-abcdef",
+                ResultOnDescribeOnlyPatchMatches = "v2.0.1-2-abcdef"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0.1", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(0, version.Minor);
+            Assert.Equal(2, version.Patch);
+            Assert.Equal(2, version.PreReleaseNumber);
+            Assert.Equal(2, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+
+        [Fact]
+        public void AfterPatchReleaseAfterMajorRelease_MinorIncrement()
+        {
+            VersioningConfig versioningConfig = new();
+            static BranchVersioning branchConfig(string _) => new() { IncrementPatch = false };
+            MockedGitExecutor git = new(versioningConfig)
+            {
+                CurrentBranch = "main",
+                HasCleanWorkingCopy = true,
+                ResultOnSimpleDescribe = "v2.0.1-2-abcdef",
+                ResultOnDescribeOnlyMinorMatches = "v2.0-5-abcdef"
+            };
+            Versioning versioning = new(versioningConfig, branchConfig, git);
+            var version = versioning.GetProjectVersion();
+
+            Assert.Equal("v2.0", version.BasedOnGitTag);
+            Assert.Equal(2, version.Major);
+            Assert.Equal(1, version.Minor);
+            Assert.Equal(0, version.Patch);
+            Assert.Equal(5, version.PreReleaseNumber);
+            Assert.Equal(5, version.Revision);
+            Assert.Equal("main", version.PreReleaseTag);
+            Assert.False(version.IsDevMark);
+        }
+    }
+}
